71574749a709550075e1a0a0b1bcf64638126e4d
theendie <andressacruzn@gmail.com>
2024-09-13 00:27:56 -0300

    post para salvar nova medalha

--- controller.py
+++ controller.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 import pandas as pd
+import uuid
 
 app = Flask(__name__)
 api = Api(app)
@@ -19,12 +20,49 @@ class Medals(Resource):
             return {'message': 'Country not found'}, 400
         return {'message': 'Country not found'}, 400
 
-    def post(self, item_id):
-        if item_id in data:
-            return {'message': 'Item already exists'}, 400
-        data[item_id] = request.json.get('value')
+    def post(self):
+        medal = request.json
+        print('Adicionando nova medalha', medal)
 
-        return {item_id: data[item_id]}, 201
+        # Pega os dados da nova medalha
+        code = str(uuid.uuid4())
+        medal_type = medal['medal_type']
+        medal_code = medal['medal_code']
+        medal_date = medal['medal_date']
+        name = medal['name']
+        gender = medal['gender']
+        discipline = medal['discipline']
+        event = medal['event']
+        event_type = medal['event_type']
+        url_event = medal['url_event']
+        country = medal['country']
+        country_long = medal['country_long']
+        country_code = medal['country_code']
+        
+        # Adiciona a nova medalha ao DataFrame
+        new_country_df = pd.DataFrame({
+        u'code': [code],
+        'medal_type': [medal_type],
+        'medal_code': [medal_code],
+        'medal_date': [medal_date],
+        'name': [name],
+        'gender': [gender],
+        'discipline': [discipline],
+        'event': [event],
+        'event_type': [event_type],
+        'url_event': [url_event],
+        'country': [country],
+        'country_long': [country_long],
+        'country_code': [country_code]
+        })
+        # Concatena o novo DataFrame com o DataFrame atual
+        self.df = pd.concat([self.df, new_country_df], ignore_index=True)
+        
+        # Salva o DataFrame atualizado no arquivo CSV
+        self.df.to_csv("dataset/medals.csv", index=False, encoding='utf-8')
+
+        return {'message': 'Medal was successfully saved.'}, 201
+
 
     def put(self, item_id):
         data[item_id] = request.json.get('value')
