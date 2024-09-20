7f903a7b202c0cd1d85c4754a845737fc7a145f8
VitoriaGonzalez <vitoria.gonzalez@poatek.com>
2024-09-20 14:46:51 -0300

    troca da porta

--- controller.py
+++ controller.py
@@ -67,4 +67,5 @@ def delete_medal():
     return {'message': 'Medal not found'}, 404
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
